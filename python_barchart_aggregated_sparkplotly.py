524e94d41a7ab3f91f3c3b894a12ac22453a6326
johnmlhll <johnmlhll@yahoo.com>
2016-03-04 16:02:45 +0000

    BarChart with Ployly stylings for a distributed SearchVSales visualation from a dataset sampled through Apache Spark

---
+++ python_barchart_aggregated_sparkplotly.py
@@ -0,0 +1,83 @@
+#Produce a Bar Chart comparing by month internet site searches and actual sales
+#from an aggregrated click stream dataset.
+#Each space denotes a different cell in Jupyter Notebooks.
+
+#figure style
+%pylab inline
+
+#import libraries
+import dataiku
+import dataiku.spark as dkuspark
+import pyspark
+from pyspark.sql import SQLContext
+import plotly.offline as py
+import plotly.graph_objs as go
+import pandas as pd
+
+#initalise plotly offline notebook
+py.init_notebook_mode()
+
+#initalise Pyspark and assign a single SQL Context(sc)
+sc = pyspark.SparkContext()
+sqlContext = SQLContext(sc)
+
+#read in dataset from Dataiku
+intCS_ds = dataiku.Dataset("MyInternetClickStreamDataSet")
+
+#assign to pyspark df
+booking_search_df = dkuspark.get_dataframe(sqlContext, booking_search_ds)
+
+#instantiate toPandas as a single call df
+booking_search_df_pandas = booking_search_df.toPandas()
+
+#declare coordinates and map the plot
+trace1 = go.Bar(
+    x = booking_search_df_pandas['search_ymd_parsed_month'],
+    y = booking_search_df_pandas['search_qty_sum'],
+    name = 'Search Totals',
+    marker=dict(
+        color='rgb(26,118,255)'
+    )
+),
+trace2 = go.Bar(
+    x = booking_search_df_pandas['search_ymd_parsed_month'],
+    y = booking_search_df_pandas['booking_qty_sum'],
+    name = 'Booking Totals',
+    marker=dict(
+        color='rgb(55,83,109)'
+    )
+)
+data=[trace1, trace2]
+
+#map the layout and styling features (if using Jupyter notebooks, code this in a new cell!)
+layout=go.Layout(
+title = 'Search to Bookings - 2015',
+    xaxis=dict(
+    tickfont=dict(
+        size = 14,
+        color = 'rgb(107,107,107)'
+        )
+    ),
+    yaxis=dict(
+    titlefont=dict(
+        size = 16,
+        color = 'rgb(107,107,107)'
+    ),
+    tickfont=dict(
+        size=14,
+        color = 'rgb(107,107,107)'
+        )
+    ),
+    legend=dict(
+        x=0,
+        y=1.0,
+        bgcolor='rgb(255,255,255,0)'
+    ),
+    barmode = 'group',
+    bargap = 0.15,
+    bargroupgap=0.1
+)
+fig = go.Figure(data=data,layout=layout)
+
+#execute the plot of the graph
+plot_url = py.iplot(fig)
