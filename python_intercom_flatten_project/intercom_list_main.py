00f9386d18bbe0da21e2d69f614561fc5f781d09
johnmlhll <johnmlhll@yahoo.com>
2016-04-21 18:07:21 +0100

    Pre Build File Version - 2 of 3 Classes Altered to Completion

--- python_intercom_flatten_project/intercom_list_main.py
+++ python_intercom_flatten_project/intercom_list_main.py
@@ -5,7 +5,7 @@
 
 #Import Classes for calling class methods
 from intercom_list_flattener import Intercom_List_Flattener
-from intercom_test_class import Intercom_Test_Class
+
 
 #Main Class
 class Intercom_Main_Flatten_Arrays(object):
@@ -13,20 +13,11 @@ class Intercom_Main_Flatten_Arrays(object):
         self.stacked_array_inputs = stacked_array_inputs
         self.flatten = flatten
 
-        #call tests - dev only
-        #instantiate test class
-        test = Intercom_Test_Class()
-
-        #call test
-        test.test_flatten_lists
-        test.test_flatten_outputs
-
         #instantiate list flattener class
         flat = Intercom_List_Flattener()
 
         #method call
         flat.flatten
-
 
     if __name__=='__main__':
         print 'Your flattened list of lists sir...'
--- python_intercom_flatten_project/intercom_test_class.py
+++ python_intercom_flatten_project/intercom_test_class.py
@@ -2,28 +2,27 @@
 
 #Import libraries
 import unittest
-
 #Class Arrays/Lists
 stacked_array_inputs = [4,2,100,[1,82,302,1],2,3,4,[411,99089,5],312,[0, 33]]
+processed_list = [4, 2, 100, 1, 82, 302, 1, 2, 3, 4, 411, 99089, 5, 312, 0, 33]
 
 class Intercom_Test_Class(unittest.TestCase):
-    #init method
-    def __init__(self, test_flatten_lists, test_flatten_outputs):
-        self.test_flatten_lists = test_flatten_lists
-        self.test_flatten_outputs = test_flatten_outputs
 
     #Testing stacked_array_inputs for the speed of list flattening
     def test_flatten_lists(self):
+        self.setUp()
         test_list = [4,2,100,[1,82,302,1],2,3,4,[411,99089,5],312,[0, 33]]
-        try:
-            self.assertTrue(type(stacked_array_inputs) == int)
-            self.assertListEqual(stacked_array_inputs, test_list)
-            print "Test passed"
-        except Exception as e:
-            print "Test failed: Error is ", e.message
-
+        self.assertTrue(type(stacked_array_inputs) == list)
+        self.assertListEqual(stacked_array_inputs, test_list)
+        self.tearDown()
     #Testing the output to ensure its correct as a list of numbers
     def test_flatten_outputs(self):
+        self.setUp()
         test_output = [4, 2, 100, 1, 82, 302, 1, 2, 3, 4, 411, 99089, 5, 312, 0, 33]
-        self.assertTrue(type(processed_list) == int)
-        self.assertListEqual(processed_output, test_output)
+        self.assertEqual(processed_list, test_output)
+        self.assertTrue(type(processed_list) == list)
+        self.assertListEqual(processed_list, test_output)
+        self.tearDown()
+
+if __name__ == '__main__':
+    unittest.main()
